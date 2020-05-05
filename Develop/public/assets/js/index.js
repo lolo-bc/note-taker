6efc37162eb2f1a5d5cd74716a2151242ee9bb87
lolobc <lauren.n.cameron@gmail.com>
2020-05-05 13:37:40 -0400

    push

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -8,6 +8,8 @@ var $noteList = $(".list-container .list-group");
 // activeNote is used to keep track of the note in the textarea
 var activeNote = {};
 
+alert("what is going on?");
+
 // A function for getting all notes from the db
 var getNotes = function() {
   return $.ajax({
@@ -142,3 +144,5 @@ $noteText.on("keyup", handleRenderSaveBtn);
 
 // Gets and renders the initial list of notes
 getAndRenderNotes();
+
+
