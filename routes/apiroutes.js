7e19d06e0ee69672eee6f920bb8830fd91967782
lolobc <lauren.n.cameron@gmail.com>
2020-05-05 15:02:09 -0400

    push

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,8 +1,14 @@
-const noteData = require('/../develop/db/db.json');
+const noteData = require('../db/db.json');
 
 module.exports = function (app) {
     app.get('/api/notes', function (req, res) {
         res.json(noteData);
-    })
+    });
+
+    app.post('/api/notes', (req, res) => {
+        const newNote = req.body;
+        notes.push(newNote);
+          res.json(true);
+      });
       
 } 
