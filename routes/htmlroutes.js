b2c61639144b1f4b497dcffd5de81b880783c5b5
lolobc <lauren.n.cameron@gmail.com>
2020-05-06 13:19:37 -0400

    push

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -3,11 +3,11 @@ const path = require('path')
 module.exports = function (app) {
 
     app.use('/notes', function (req, res) {
-        res.sendFile(path.join(__dirname, '/../Develop/public/notes.html'));
+        res.sendFile(path.join(__dirname, '/../public/notes.html'));
       }); 
 
       app.use('/index', function (req, res) {
-        res.sendFile(path.join(__dirname, '/../Develop/public/index.html'));
+        res.sendFile(path.join(__dirname, '/../public/index.html'));
       }); 
 
     //   app.use(function (req, res) {
