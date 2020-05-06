4577e76f977e3ef35afccb45a95578443692a143
lolobc <lauren.n.cameron@gmail.com>
2020-05-06 12:45:41 -0400

    push

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,6 +1,8 @@
 const path = require('path')
 
 module.exports = function (app) {
+    app.use(express.static('build'));
+
     app.use('/notes', function (req, res) {
         res.sendFile(path.join(__dirname, '/../Develop/public/notes.html'));
       }); 
